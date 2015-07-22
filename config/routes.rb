2e9f64d880876aa12838841a1eaa4f10bc5b1522
myanchar <michael.yanchar@gmail.com>
2015-07-22 14:49:48 -0700

    Added basic stripe functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "/log-out" => "sessions#destroy", as: :log_out
 
   resources :users
+  resources :charges
   resources :scholarships do
     resources :donations
   end
