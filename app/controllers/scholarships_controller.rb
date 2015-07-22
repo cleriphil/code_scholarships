6f808d2df5cf0c4bbb5c921af6c7cdc1b87ff623
cleri philiastides & kim dang <student@epicodus.com>
2015-07-22 14:05:54 -0700

    Users can donate to scholarships.

--- app/controllers/scholarships_controller.rb
+++ app/controllers/scholarships_controller.rb
@@ -3,6 +3,10 @@ class ScholarshipsController < ApplicationController
     @scholarships = Scholarship.all
   end
 
+  def show
+    @scholarship = Scholarship.find(params[:id])
+  end
+
   def new
     @user = current_user
     @scholarship = @user.scholarships.new
@@ -12,17 +16,17 @@ class ScholarshipsController < ApplicationController
     @user = current_user
     @scholarship = @user.scholarships.new(scholarship_params)
     if @scholarship.save()
-      flash[:notice] = "Your scholarship as been added!"
-      redirect_to root_path
+      flash[:notice] = "Your scholarship has been added!"
+      redirect_to scholarships_path
     else
-      flash[:flash] = "There was a problem with your submission. Please try again."
+      flash[:alert] = "There was a problem with your submission. Please try again."
       render :new
     end
   end
 
   private
   def scholarship_params
-    params.require(:scholarship).permit(:amount_requested, :description, :study_type)
+    params.require(:scholarship).permit(:amount_requested, :description, :study_type, :title)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722184556) do
+ActiveRecord::Schema.define(version: 20150722203852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150722184556) do
     t.string  "description"
     t.string  "study_type"
     t.integer "user_id"
+    t.string  "title"
   end
 
   create_table "users", force: :cascade do |t|
