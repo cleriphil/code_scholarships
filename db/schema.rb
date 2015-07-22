503c402e683c06d28f72031212c1c4519bced88c
cleri philiastides & kim dang <student@epicodus.com>
2015-07-22 10:28:04 -0700

    pass scholarship spec

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722171747) do
+ActiveRecord::Schema.define(version: 20150722172429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20150722171747) do
   create_table "scholarships", force: :cascade do |t|
     t.integer "amount_requested", default: 0
     t.integer "amount_fulfilled", default: 0
-    t.integer "interest_rate",    default: 0
     t.string  "description"
     t.string  "type"
     t.integer "user_id"
--- spec/models/scholarship_spec.rb
+++ spec/models/scholarship_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 
 describe Scholarship, type: :model do
-  # it { should have_many :donations }
+  it { should have_many :donations }
   # it { should have_many :users }
-  # it { should validate_presence_of :amount_requested }
+  it { should validate_presence_of :amount_requested }
   it { should validate_presence_of :amount_fulfilled }
   it { should validate_presence_of :description }
   it { should validate_presence_of :type }
