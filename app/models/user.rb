23f73ac95e07c334c6b7e2d7574f7d43231b14ce
Guest User <student@epicodus.com>
2015-07-23 11:40:08 -0700

    add paperclip default avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   attr_accessor :password
   validates_confirmation_of :password
   before_save :encrypt_password
+  validates :username, :uniqueness => true
   validates :name, :presence => true
   validates :password, :presence => true
   validates :username, :presence => true
@@ -13,7 +14,7 @@ class User < ActiveRecord::Base
   has_many :scholarships
   has_many :donations
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :mini => "30x30", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/smiley.jpg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   def encrypt_password
