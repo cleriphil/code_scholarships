84457b4312d06891a85190ac965fe0d0ff5f241d
ian bevel & bob ellis <student@epicodus.com>
2015-07-24 11:27:31 -0700

    add progress bar for repayment, add payments index with table, round floats to whole numbers

--- app/controllers/repayments_controller.rb
+++ app/controllers/repayments_controller.rb
@@ -1,5 +1,7 @@
 class RepaymentsController < ApplicationController
   def index
+    @scholarship = Scholarship.find(params[:scholarship_id])
+    @repayments = @scholarship.repayments
   end
 
   def new
@@ -12,15 +14,20 @@ class RepaymentsController < ApplicationController
     @user = current_user
     @scholarship = Scholarship.find(params[:scholarship_id])
     @repayment = @scholarship.repayments.new(repayment_params)
-    @repayment.user_id = current_user.id
-    @repayment.scholarship_id = @scholarship.id
-    @user.repayments.push(@repayment)
-    if @repayment.save
-      flash[:notice] = "Repayment has been submitted!"
-      redirect_to new_repayment_charge_path(@repayment)
+    if @repayment.amount > (@scholarship.amount_owed - @scholarship.amount_repaid)
+      flash[:alert] = "Payment amount is higher that the amount owed! Dont pay more than you owe!"
+      redirect_to new_scholarship_repayment_path(@scholarship)
     else
-      flash[:alert] = "There was a problem submitting your repayment."
-      render :new
+      @repayment.user_id = current_user.id
+      @repayment.scholarship_id = @scholarship.id
+      @user.repayments.push(@repayment)
+      if @repayment.save
+        flash[:notice] = "Repayment has been submitted!"
+        redirect_to new_repayment_charge_path(@repayment)
+      else
+        flash[:alert] = "There was a problem submitting your repayment."
+        render :new
+      end
     end
   end
 
