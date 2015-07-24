96334c2971ff30b5b60ae2946363299f37e2447b
myanchar <michael.yanchar@gmail.com>
2015-07-23 23:48:03 -0700

    Added dancing robot. Restyled forms and reconfigured routes with new pages. Added fade effect to alert text.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,3 +33,8 @@ $(document).scroll(function() {
   }
 });
 
+$(function() {
+   $('#flash').delay(300).fadeIn('normal', function() {
+      $(this).delay(5000).fadeOut();
+   });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get '/' => 'home#landing'
+  get '/home/secret' => 'home#secret'
   get "/log-in" => "sessions#new"
   post "/log-in" => "sessions#create"
   get "/log-out" => "sessions#destroy", as: :log_out
 
   resources :users
-  resources :home, only: [:index, :landing]
+  resources :home, only: [:index, :landing, :secret]
 
   resources :scholarships do
     resources :donations
